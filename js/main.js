edf1e2fbff22e893beb8f88778ffd9e948f14288
Oona <oona.e.rauhala@gmail.com>
2019-04-03 19:08:09 +0300

    '[q]'

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@
 const form = document.querySelector('form');
 const allInputs = document.querySelectorAll('input');
 
-//submit is also input, can be changed to button
 
 let valid = 0; //counts valid inputs
 
+//checkEmpty checks if value is empty
 const checkEmpty = element => {
   if (element.value === '') {
     //not valid, set border red
@@ -15,27 +15,19 @@ const checkEmpty = element => {
     console.log('CheckEmpty empty');
   } else {
     //add valid counter +1
-    valid = valid +1;
+    valid = valid + 1;
     console.log('CheckEmpty not empty');
   }
 };
 
-//function checkPattern if patterns match (these 2 functions enough)
+//checkPattern checks if patterns match
 const checkPattern = element => {
 
-
-
-
-
   const regex = RegExp(element.getAttribute('pattern'));
-  console.log(regex.toString())
 
-
-
-
-  if (regex.test(element.value)===true) {
+  if (regex.test(element.value) === true) {
     //add valid counter +1
-    valid = valid +1;
+    valid = valid + 1;
     console.log('CheckPattern pattern valid');
   } else {
     // not valid, set border red
@@ -51,28 +43,19 @@ form.addEventListener('submit', (evt) => {
   console.log('Prevented submit default');
 
   for (let i = 0; i < allInputs.length; i++) {
-    console.log(` \nLoop num ${i+1}`);
-    console.log(allInputs[i]);
-
-
-    console.log(`Input was: ${allInputs[i].value}`);
-
+    console.log(` \nLoop num ${i + 1}`);
 
     if (allInputs[i].hasAttribute('required')) {
       checkEmpty(allInputs[i]);
-      console.log(`Loop num ${i}: if hasAttribute 'required': checkEmpty DONE`);
     }
     if (allInputs[i].hasAttribute('pattern')) {
       checkPattern(allInputs[i]);
-      console.log(
-          `Loop num ${i}: if hasAttribute 'pattern': checkPattern DONE`);
     }
 
-  //when validation ok (valid = 8)
-  if (valid === 8) {
-    form.submit();
-  } else {
-    //default already prevented, do nothing
+    //when validation ok (valid = 8)
+    if (valid === 8) {
+      form.submit();
+    }
+    console.log(`Valid = ${valid}`);
   }
-  console.log(`Valid = ${valid}`);
-}});
+});
